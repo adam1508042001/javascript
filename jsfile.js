4fc802c6cbf2512ece9f9cd06670733c3332eba0
ademo <afroune.2001.mohamed@outlook.com>
2024-08-02 00:51:30 +0200

    Reapply "premier exo , calcul de la bmi de john et mark"
    This reverts commit 31d26ec8f8cf9d0db686747bf853d423ea9ed4db.

---
+++ jsfile.js
@@ -0,0 +1,19 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('javascript is funny!')
+
+
+console.log(40+80+3-23);
+
+/* it will be in the console */
+
+
+//declarer une variable 
+let firtName = "zkr";
+console.log(firtName);
+console.log("my first name is " + firtName);
+
+
+let second_name = "zakaria";
+console.log("and my secode name is " + second_name);
+
+
